f02176e85fa7c31bffe5c0693196493e16042025
virmon <virmontumulak@gmail.com>
2020-11-25 15:13:33 -0600

    Add Middlewares

---
+++ src/middleware/logger.js
@@ -0,0 +1,10 @@
+const logger = (store) => (next) => (action) => {
+    console.group(action.type)
+    console.log('The action', action)
+    const retVal = next(action)
+    console.log('The State', store.getState())
+    console.groupEnd()
+    return retVal
+}
+
+export default logger
